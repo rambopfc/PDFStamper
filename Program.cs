7017207c516413fcb64f50f2a11275b6777823ee
Joe <JoeBotta@gmail.com>
2023-01-24 16:37:25 -0500

    Changed arguments handling to handle multiple document types. Added Lab Notebook with barcode.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Windows.Forms;
 using iText.IO.Font.Constants;
 using iText.Kernel.Font;
+using iText.Kernel.Pdf.Xobject;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas;
@@ -10,6 +11,7 @@ using iText.Kernel.Colors;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
+using iText.Barcodes;
 
 namespace PDFStamper
 {
@@ -17,6 +19,7 @@ namespace PDFStamper
     {
         static int Main(string[] args)
         {
+            #region ArgsCheck
             //Program was run with no / not enough input. Show help menu.
             if (args.Length == 0 || args.Length < 7)
             {
@@ -25,171 +28,196 @@ namespace PDFStamper
             }
             else
             {
-                if ((args[0] == "/h") || (args[0] =="-h") || (args[0] == "help"))
+                if ((args[0] == "/h") || (args[0] == "-h") || (args[0] == "help"))
                 {
                     Intro();
                     return 0;
                 }
             }
+            #endregion
 
+            string Pagetype;
+            string DataPacketID;
+            string ClientName;
+            string ProductName;
+            string DocID;
+            string Barcode;
+            int PDFPageCount;
+            string ReportedName;
+            bool IsHorizontal;
+            int PageNumStart;
+            string footertext = "ANALYST INITIALS & DATE:_____________________   REVIEWER INITIALS & DATE:_____________________    PAGE: ";
             try
             {
-                string filepath = args[0];
-                FileInfo inputfile = new FileInfo(filepath);
-                string pathout = args[1];
-                FileInfo outputfile = new FileInfo(pathout);
-                outputfile.Delete();
-
-
-                string DataPacketID;
-                string docid;
-                bool isHorizontal;
-                string ReportedName;
-
+                //Check page type first as this will determine what other args to look for. If this is missing or invalid nothing else matters.
+                //First and Second args should always be input/output file path.
+                
                 if (string.IsNullOrWhiteSpace(args[2].ToString()))
                 {
-                    throw new Exception("Project Number is invalid!");
+                    throw new Exception("Page type is invalid!");
                 }
                 else
                 {
-                    DataPacketID = args[2].ToString();
+                    Pagetype = args[2].ToString();
                 }
-
-
-                if (!int.TryParse(args[3], out _))
-                {
-                    throw new Exception("Page number is not a number!");
-                }
-
-                string footertext = "ANALYST INITIALS & DATE:_____________________   REVIEWER INITIALS & DATE:_____________________    PAGE: " + args[3].ToString();
-
+                string filepath = args[0];
+                FileInfo inputfile = new FileInfo(filepath);
                 if (!inputfile.Exists)
                 {
                     throw new Exception("Input file not found!");
                 }
+                string pathout = args[1];
+                FileInfo outputfile = new FileInfo(pathout);
+                outputfile.Delete();
 
-                if (args[4].ToString() == "T")
+                if (string.IsNullOrWhiteSpace(args[3].ToString()))
                 {
-                    isHorizontal = true;
-                }
-                else if (args[4].ToString() == "F")
-                {
-                    isHorizontal = false;
+                    throw new Exception("Datapacket ID not found!");
                 }
                 else
                 {
-                    throw new Exception("Horizontal check failed!");
+                    DataPacketID = args[3].ToString();
+                }
+
+                if (!int.TryParse(args[4], out PageNumStart))
+                {
+                    throw new Exception("Page number is invalid!");
                 }
 
                 if (string.IsNullOrWhiteSpace(args[5].ToString()))
                 {
-                    throw new Exception("Doc ID and version were invalid!");
+                    throw new Exception("Reported name not found!");
                 }
                 else
                 {
-                    docid = args[5].ToString();
+                    ReportedName = args[5].ToString();
                 }
 
                 if (string.IsNullOrWhiteSpace(args[6].ToString()))
                 {
-                    throw new Exception("Test reported name was invalid!");
+                    throw new Exception("Doc ID is invalid");
                 }
                 else
                 {
-                    ReportedName = args[6].ToString();
+                    DocID = args[6].ToString();
                 }
-
-                
 
                 //Initialize PDF document
                 PdfReader reader = new PdfReader(inputfile);
                 //This has to be set to true for un-passworded docs because PDF's are dumb
                 reader.SetUnethicalReading(true);
                 PdfDocument pdfDoc = new PdfDocument(reader, new PdfWriter(outputfile));
-                PdfCanvas canvasWrite = new PdfCanvas(pdfDoc.GetFirstPage());
-                PdfPage page = pdfDoc.GetFirstPage();
-                Rectangle stuff = page.GetPageSizeWithRotation();
-                if (stuff.GetHeight() >= stuff.GetWidth())
+                PDFPageCount = pdfDoc.GetNumberOfPages();
+
+
+                //Valid page types
+                //LNBC - Lab Notebook Cover Page
+                //ENBC - Equipment Notebook Cover Page
+                //NBP - Regular notebook page
+                switch (Pagetype)
                 {
-                    //page is portrait
+                    case "LNBC":
+
+                        #region ArgsSetup
+                        if (string.IsNullOrWhiteSpace(args[7].ToString()))
+                        {
+                            throw new Exception("Barcode string not found!");
+                        }
+                        else
+                        {
+                            Barcode = args[7].ToString();
+                        }
+
+                        if (string.IsNullOrWhiteSpace(args[8].ToString()))
+                        {
+                            throw new Exception("Client name not found!");
+                        }
+                        else
+                        {
+                            ClientName = args[8].ToString();
+                        }
+
+                        if (string.IsNullOrWhiteSpace(args[9].ToString()))
+                        {
+                            throw new Exception("Product name not found!");
+                        }
+                        else
+                        {
+                            ProductName = args[9].ToString();
+                        }
+                        #endregion
+
+                        #region LNBC Stamps
+                        PdfCanvas canvasWrite = new PdfCanvas(pdfDoc.GetFirstPage());
+                        PdfPage page = pdfDoc.GetFirstPage();
+
+                        Paragraph p = new Paragraph(ReportedName).SetFontSize(10);
+                        p.SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA));
+                        p.SetRelativePosition(125, 8, 100, 100);
+                        p.SetMaxWidth(325);
+                        new Canvas(page, page.GetPageSize()).Add(p).Close();
+                        canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(475, 770
+                        ).ShowText(DataPacketID).EndText();
+                        canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(475, 755
+                        ).ShowText("Date:_______________").EndText();
+                        canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 6).MoveText(10, 775
+                        ).ShowText(DocID).EndText();
+
+                        //vertical footer
+                        canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(10, 15
+                           ).ShowText(footertext + (PageNumStart + 1)).EndText();
+
+
+                        Paragraph title = new Paragraph("Laboratory Notebook Cover Page").SetFontSize(15);
+                        title.SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA));
+                        title.SetRelativePosition(190,40,100,100);
+                        title.SetMaxWidth(250);
+                        new Canvas(page, page.GetPageSize()).Add(title).Close();
+
+                        Barcode128 barcode128 = new Barcode128(pdfDoc);
+                        barcode128.SetCodeType(Barcode128.CODE_C);
+                        barcode128.SetCode(Barcode);
+                        barcode128.SetBarHeight(15);
+                        PdfFormXObject barcodeimg = barcode128.CreateFormXObject(ColorConstants.BLACK, ColorConstants.BLACK, pdfDoc);
+                        canvasWrite.AddXObjectAt(barcodeimg, 275, 700);
+
+                        canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(10, 695
+                        ).ShowText("Client: " + ClientName).EndText();
+                        canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(10, 675
+                        ).ShowText("Product: " + ProductName).EndText();
+
+                        break;
+                    #endregion
+
+                    #region Equipment NB Stamps
+                    case "ENBC":
+
+                        if (string.IsNullOrWhiteSpace(args[7].ToString()))
+                        {
+                            throw new Exception("Barcode string not found!");
+                        }
+                        else
+                        {
+                            Barcode = args[7].ToString();
+                        }
+                        //Add other Equipment NB stuff here
+
+                        break;
+                    #endregion
+
+                    case "NBP":
+                        //So far nothing special goes here?
+
+                        break;
+                    default:
+                        throw new Exception("Invalid page type!");
+                        
                 }
-                else
-                {
-                    //page is landscape
-                }
-
-                #region Header
-
-                if (isHorizontal)
-                {
-
-                }
-                else
-                {
-
-                    Paragraph p = new Paragraph(ReportedName).SetFontSize(10);
-                    p.SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA));
-                    p.SetRelativePosition(125, 8, 100, 100);
-                    p.SetMaxWidth(325);
-                    new Canvas(page, page.GetPageSize()).Add(p).Close();
-
-
-                    canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(475, 770
-                    ).ShowText(DataPacketID).EndText();
-                    canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(475, 755
-                    ).ShowText("Date:_______________").EndText();
-                    canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(495, 739
-                    ).ShowText("Page Not Used").EndText();
-                    canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 6).MoveText(10, 775
-                    ).ShowText(docid).EndText();
-                    Rectangle rekt = new Rectangle(565, 735, 15, 15);
-                    canvasWrite.Rectangle(rekt);
-                    canvasWrite.Stroke();
-
-
-                }
-
-                #endregion
-
-                
-
-
-
-                #region Footer
-                if (isHorizontal)
-                {
-                    Paragraph p = new Paragraph(footertext).SetFontSize(10);
-                    p.SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA));
-                    p.SetRotationAngle(-146.087);
-                    p.SetFixedPosition(8, 750, 1000);
-                    new Canvas(page, page.GetPageSize()).Add(p).Close();
-
-
-                }
-                else
-                {
-                    //vertical footer
-                    canvasWrite.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 10).MoveText(10, 15
-                       ).ShowText(footertext).EndText();
-                }
-                #endregion
-
-
-
-
-
-                //Use this for horizontal pages
-                //Paragraph p = new Paragraph("CONFIDENTIAL").SetFontSize(60);
-                //p.SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA));
-                //p.SetRotationAngle(90);
+ 
 
 
 
                 pdfDoc.Close();
-
-                
-
-
                 return 1;
             }
             catch (System.IndexOutOfRangeException) 
